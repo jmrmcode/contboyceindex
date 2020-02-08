07043782f0fd3c7ca185094ac2330f0163cd4f07
Juan Miguel Requena Mullor <juanmimullor@gmail.com>
2020-02-08 16:45:36 -0700

    Update C_BoyceIndex.R

--- C_BoyceIndex.R
+++ C_BoyceIndex.R
@@ -22,23 +22,23 @@
 
 # Note: the script assumes that the input matrix is called "data".
 
-library("abind")
+require("abind")
 
 w<-0.01#window size. This parameter is adjustable by user (see Hirzel et al. 2006)
 vectori<-NULL
 boyce<-NULL
 for (i in seq(0,0.99,w)){
-  pitotal<-length(which(data$presence==1))#total number of evaluation presences
-  pipresence<-length(which(data$presence==1&data$suitability>=i&data$suitability<(i+w)))#number of evaluation points predicted by the model to fall in the habitat suitability class i
-  aitotal<-length(data$suitability)#overall number of cells in the whole study area
-  aicategory<-length(which(data$suitability>=i&data$suitability<(i+w)))#number of grid cells belonging to the habitat suitability class i
+  pitotal<-length(which(data$presence==1))#total number of presences
+  pipresence<-length(which(data$presence==1&data$suitability>=i&data$suitability<(i+w)))#number of observations predicted by the model to fall in the habitat suitability class i
+  aitotal<-length(data$suitability)#overall number of observations in the data set
+  aicategory<-length(which(data$suitability>=i&data$suitability<(i+w)))#number of observations belonging to the habitat suitability class i
   
   Pi<-pipresence/pitotal#predicted frequency
   Ei<-aicategory/aitotal#expected frequency
   Fi<-Pi/Ei#predicted-to-expected (P/E) ratio Fi
   
   vectori<-as.array(Fi)#put Fi into an array
-  boyce<-abind(boyce,vectori,along=)#join the array created in the previous step and the current one
+  boyce<-abind(boyce,vectori,along=)# join it to the array created in the previous step 
 }
 boyce_index<-cor.test(seq(0,0.99,0.01),boyce,method="spearman")
 boyce_index
