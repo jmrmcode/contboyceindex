d1f2ee33995c0a326a8b3b1badc01735c897c69d
Juan Miguel Requena Mullor <juanmimullor@gmail.com>
2020-11-05 08:14:08 -0500

    Update_BoyceIndex
    Simulate the input data structure (lines 26-27), suppress warning message in cor.test function, connect seq() with "w" (line 46), print just Boyce index (47)

--- C_BoyceIndex.R
+++ C_BoyceIndex.R
@@ -17,28 +17,31 @@
 # Evaluating resource selection functions. Ecol. Model. 157,281-300
 
 #####################
-# The input data must be a matrix with two columns called "suitability" (i.e., logistic output from the model) 
+# The input data must be a data frame with two columns called "suitability" (i.e., logistic output from the model) 
 # and "presence" (i.e., one option for each location-pixel: 1 or 0).
 
-# Note: the script assumes that the input matrix is called "data".
+# Note: the script assumes that the input data frame is called "data".
 
-require("abind")
+# Simulate input data frame
+data <- data.frame(suitability = rbeta(100, shape1 = 1, shape2 = 0.5), 
+        presence = rbinom(100, 1, 0.6))
 
-w <- 0.01# window size. This parameter is adjustable by user (see Hirzel et al. 2006)
+# compute Boyce index
+w <- 0.01 # window size. This parameter is adjustable by user (see Hirzel et al. 2006)
 vectori <- NULL
 boyce <- NULL
 for (i in seq(0, 0.99, w)){
-  pitotal <- length(which(data$presence==1))# total number of presences
-  pipresence <- length(which(data$presence==1&data$suitability>=i&data$suitability<(i+w)))# number of observations predicted by the model to fall in the habitat suitability class i
-  aitotal <- length(data$suitability)# overall number of observations in the data set
-  aicategory <- length(which(data$suitability>=i&data$suitability<(i+w)))# number of observations belonging to the habitat suitability class i
+  pitotal <- length(which(data$presence==1)) # total number of presences
+  pipresence <- length(which(data$presence==1&data$suitability>=i&data$suitability<(i+w))) # number of observations predicted by the model to fall in the habitat suitability class i
+  aitotal <- length(data$suitability) # overall number of observations in the data set
+  aicategory <- length(which(data$suitability>=i&data$suitability<(i+w))) # number of observations belonging to the habitat suitability class i
   
-  Pi <- pipresence / pitotal# predicted frequency
-  Ei <- aicategory / aitotal# expected frequency
-  Fi <- Pi / Ei# predicted-to-expected (P/E) ratio Fi
+  Pi <- pipresence / pitotal # predicted frequency
+  Ei <- aicategory / aitotal # expected frequency
+  Fi <- Pi / Ei # predicted-to-expected (P/E) ratio Fi
   
-  vectori <- as.array(Fi)# put Fi into an array
-  boyce <- abind(boyce, vectori, along=)# join it to the array created in the previous step 
+  vectori <- as.array(Fi) # put Fi into an array
+  boyce <- abind(boyce, vectori, along=) # join it to the array created in the previous step 
 }
-boyce_index <- cor.test(seq(0, 0.99, 0.01), boyce, method="spearman")
-boyce_index
+boyce_index <- suppressWarnings({cor.test(seq(0, 0.99, w), boyce, method="spearman")})
+boyce_index$estimate
